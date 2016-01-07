68869891fbc6819cdb5dd3ee7548c97d5f3766fb
马文铂 <luoyi@uoko.com>
2016-01-07 17:56:54 +0800

    *  test

--- OneSwiftProject/OneSwiftProject/MainViewController.swift
+++ OneSwiftProject/OneSwiftProject/MainViewController.swift
@@ -8,33 +8,19 @@
 
 import UIKit
 
+import ReactiveCocoa
+
 class MainViewController: UIViewController {
 
     @IBAction func pushButton(sender: UIButton) {
         
         self.navigationController?.pushViewController(UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ViewController"), animated: false)
         
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
